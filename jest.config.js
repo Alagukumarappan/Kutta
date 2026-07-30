27883fc0980256bfb7d347993ea5985627962894
Alagukumarappan Ramasamy <alagukumarappan.ramasamy@ionos.com>
2026-07-30 14:03:55 +0200

    fix: restore jest-expo preset and fix babel-preset-expo module resolution

--- jest.config.js
+++ jest.config.js
@@ -1,8 +1,8 @@
 module.exports = {
-  testEnvironment: 'node',
-  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
-  transform: {
-    '^.+\\.tsx?$': 'ts-jest',
-  },
+  preset: 'jest-expo',
+  transformIgnorePatterns: [
+    'node_modules/(?!((jest-)?react-native|@react-native(-community)?)|expo(nent)?|@expo(nent)?/.*|@expo-google-fonts/.*|react-navigation|@react-navigation/.*|@unimodules/.*|unimodules|sentry-expo|native-base|react-native-svg)',
+  ],
+  setupFilesAfterEnv: [],
   testPathIgnorePatterns: ['/node_modules/', '/android/', '/ios/'],
 };
